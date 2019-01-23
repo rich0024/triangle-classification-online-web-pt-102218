aac693a761878fe9c48ba5da4dff29d7faa2c3d1
Richard Yance <yance0022@gmail.com>
2019-01-23 05:42:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,39 +1,31 @@
 class Triangle
-  # write code here
-attr_accessor :side_A, :side_B, :side_C
 
-  def initialization(side_A, side_B, side_C)
-    @side_A = side_A
-    @side_B = side_B
-    @side_C = side_C
-    @sides = []
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
-    valid?
-    if (side_A == side_B) && (side_B == side_C) && (side_C == side_A)
+    validate_triangle
+    if (a == b) && (b==c) && (c==a)
       :equilateral
-    elsif (side_A == side_B) || (side_B == side_C) || (side_C == side_A)
+    elsif (a == b) || (b==c) || (c==a)
       :isosceles
     else
       :scalene
     end
   end
 
-  def valid?
-    @sides << side_A
-    @sides << side_B
-    @sides << side_C
-  triangle = [(side_A + side_B > side_C),
-          (side_A + side_C > side_B),
-          (side_B + side_C > side_A)]
-          @sides.each { |side| real << false if side <= 0 }
-  raise TriangleError if triangle.include?(false)
+  def validate_triangle
+    real = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each { |s| real << false if s <= 0 }
+    raise TriangleError if real.include?(false)
   end
-
 
   class TriangleError < StandardError
-  # triangle error code
   end
-
+  # write code here
 end
