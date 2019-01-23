893c94ad296eabcb8a7f6213b64737cab507e6be
Richard Yance <yance0022@gmail.com>
2019-01-23 05:54:44 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,7 @@
 class Triangle
 
   attr_accessor :side_A, :side_B, :side_C
-  
+
     def initialization(side_A, side_B, side_C)
       @side_A = side_A
       @side_B = side_B
@@ -21,10 +21,8 @@ class Triangle
   end
 
   def valid
-    triangle = [(side_A + side_B > side_C),
-          (side_A + side_C > side_B),
-          (side_B + side_C > side_A)]
-          [side_A, side_B, side_C].each do |side| triangle << false if side <= 0 
+    triangle = [(side_A + side_B > side_C), (side_A + side_C > side_B), (side_B + side_C > side_A)]
+          [side_A, side_B, side_C].each do |side| triangle << false if side <= 0
           end
           raise TriangleError if triangle.include?(false)
       end
